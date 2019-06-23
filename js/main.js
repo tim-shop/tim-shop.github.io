60065464afb62580f1a20b8acfc5ccd46cd09473
tim-shop <51885381+tim-shop@users.noreply.github.com>
2019-06-23 14:57:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-/*price range*/
-
- $('#sl2').slider();
-
-	var RGBChange = function() {
-	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
-	};	
+var RGBChange = function() {
+  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+};	
 		
 /*scroll to top*/
 
@@ -12,14 +8,14 @@ $(document).ready(function(){
 	$(function () {
 		$.scrollUp({
 	        scrollName: 'scrollUp', // Element ID
-	        scrollDistance: 300, // Distance from top/bottom before showing element (px)
+	        scrollDistance: 200, // Distance from top/bottom before showing element (px)
 	        scrollFrom: 'top', // 'top' or 'bottom'
-	        scrollSpeed: 300, // Speed back to top (ms)
+	        scrollSpeed: 400, // Speed back to top (ms)
 	        easingType: 'linear', // Scroll to top easing (see http://easings.net/)
 	        animation: 'fade', // Fade, slide, none
-	        animationSpeed: 200, // Animation in speed (ms)
+	        animationSpeed: 400, // Animation in speed (ms)
 	        scrollTrigger: false, // Set a custom triggering element. Can be an HTML string or jQuery object
-					//scrollTarget: false, // Set a custom target element for scrolling to the top
+		//scrollTarget: false, // Set a custom target element for scrolling to the top
 	        scrollText: '<i class="fa fa-angle-up"></i>', // Text for element, can contain HTML
 	        scrollTitle: false, // Set a custom <a> title if required.
 	        scrollImg: false, // Set true to use image
